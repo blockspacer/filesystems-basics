3c2abe48717550d871f9044b79ded7d66a6c2561
Roman Detinin <detininroman@gmail.com>
2019-11-27 11:19:32 +0300

    added random string generating

--- grpc/client.py
+++ grpc/client.py
@@ -6,15 +6,17 @@ from datetime import datetime
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
+from termcolor import colored
 
 
 def run(counter: int):
     start = datetime.now()
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='world'))
-    timedelta = round((datetime.now() - start).total_seconds() * 1000, 2)
-    print(f'[{counter}] {response.message} Time: {timedelta} ms')
+        response = stub.SayHello(helloworld_pb2.HelloRequest())
+    timedelta = round((datetime.now() - start).total_seconds() * 1000, 1)
+    color = 'red' if timedelta >= 1000 else 'green'
+    print(colored(f'[{counter}] {response.message} Time: {timedelta} ms', color))
 
 
 if __name__ == '__main__':
--- grpc/server.py
+++ grpc/server.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import string
 import time
 from concurrent import futures
 
@@ -11,12 +12,14 @@ import helloworld_pb2_grpc
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def __init__(self):
         self.counter = random.randint(1, 100)
+        self.char_set = string.ascii_letters + string.digits
 
     def SayHello(self, request, context):
         self.counter += 1
         if self.counter % 100 == 0:
             time.sleep(1)
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+        random_string = ''.join(random.choices(self.char_set, k=128))
+        return helloworld_pb2.HelloReply(message=random_string)
 
 
 def serve():

f7e6aafa16b629067e3f401c609d60cdbe7f878b
Roman Detinin <detininroman@gmail.com>
2019-11-27 13:49:52 +0300

    structural changes

--- grpc/client.py
+++ grpc/client.py
@@ -1,25 +1,22 @@
-from __future__ import print_function
-
-import logging
 from datetime import datetime
 
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
 from termcolor import colored
+
+import generator_pb2
+import generator_pb2_grpc
 
 
 def run(counter: int):
     start = datetime.now()
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest())
+        stub = generator_pb2_grpc.GeneratorStub(channel)
+        response = stub.generate(generator_pb2.GenRequest())
     timedelta = round((datetime.now() - start).total_seconds() * 1000, 1)
     color = 'red' if timedelta >= 1000 else 'green'
-    print(colored(f'[{counter}] {response.message} Time: {timedelta} ms', color))
+    print(colored(f'[{counter}] {response.text} Time: {timedelta} ms', color))
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    for i in range(1, 101):
+    for i in range(100):
         run(i)
--- grpc/server.py
+++ grpc/server.py
@@ -1,35 +1,34 @@
-import logging
 import random
 import string
 import time
 from concurrent import futures
 
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
+
+import generator_pb2
+import generator_pb2_grpc
 
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
+class Generator(generator_pb2_grpc.GeneratorServicer):
     def __init__(self):
-        self.counter = random.randint(1, 100)
+        self.counter = random.randint(0, 99)
         self.char_set = string.ascii_letters + string.digits
 
-    def SayHello(self, request, context):
+    def generate(self, request, context):
         self.counter += 1
         if self.counter % 100 == 0:
             time.sleep(1)
         random_string = ''.join(random.choices(self.char_set, k=128))
-        return helloworld_pb2.HelloReply(message=random_string)
+        return generator_pb2.GenReply(text=random_string)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    generator_pb2_grpc.add_GeneratorServicer_to_server(Generator(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()

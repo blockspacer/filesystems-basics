e7fdd46642a9803b8268ba1d64c53db2313d1a14
Roman Detinin <detininroman@gmail.com>
2019-11-30 23:40:18 +0300

    added ps implementation

---
+++ other/ps.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
+
+bool isNumber(const std::string &str) {
+    return std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+std::string getCmd(const int pid) {
+    std::ifstream src("/proc/" + std::to_string(pid) + "/cmdline");
+    std::stringstream command;
+    command << src.rdbuf();
+    return command.str();
+}
+
+std::vector<int> getPids() {
+    std::vector<int> pids;
+    for (const auto &entry : fs::directory_iterator("/proc/")) {
+        auto filename = entry.path().filename();
+        if (isNumber(filename))
+            pids.push_back(std::stoi(filename.u8string()));
+    }
+    return pids;
+}
+
+int main() {
+    std::cout << "PID" << "\t" << "CMD" << std::endl;
+    for (auto pid : getPids())
+        std::cout << pid << "\t" << getCmd(pid) << "\n";
+
+    return 0;
+}

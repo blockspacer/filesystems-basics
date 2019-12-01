a02652661733ae144ce9d93a8205d1e5a42e9118
Roman Detinin <detininroman@gmail.com>
2019-12-01 12:48:34 +0300

    minor codestyle changes

--- other/lsof.cpp
+++ other/lsof.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "./utils.h"
 
-
 int main() {
     std::cout << "CMD" << "\t" << "PID" << "\t" << "USER" << std::endl;
     for (auto pid : get_pids())
-        std::cout << get_cmd(pid) << "\t" << pid << "\t" << get_user(pid) << "\n";
+        std::cout << get_cmd(pid) << "\t" << pid << "\t" << get_user(pid) << std::endl;
 
     return 0;
 }
--- other/ps.cpp
+++ other/ps.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::cout << "PID" << "\t" << "CMD" << std::endl;
     for (auto pid : get_pids())
-        std::cout << pid << "\t" << get_cmd(pid) << "\n";
+        std::cout << pid << "\t" << get_cmd(pid) << std::endl;
 
     return 0;
 }
